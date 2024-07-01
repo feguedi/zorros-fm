c7ab1d0c899234734627d94dc180d6e7b4cf2499
Ixe Fernández <feguedi@yahoo.com.mx>
2024-07-01 05:06:08 -0500

    Configuración de Nuxt actualizada

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,47 @@
+import path from 'path';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  runtimeConfig: {
+    apiBase: import.meta.env.NUXT_API_BASE || 'http://127.0.0.1:8080',
+    apiEndpoint: import.meta.env.NUXT_API_ENDPOINT || '/api',
+  },
+  build: {
+    transpile: [
+      '@iconify-json/carbon',
+      '@iconify-json/logos',
+      '@iconify-json/svg-spinners',
+      '@iconify/utils',
+      '@iconify/vue',
+    ],
+  },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', value: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', value: '/logo.svg' },
+        { rel: 'apple-touch-icon', value: '/icons/192x192.png' },
+      ],
+    },
+  },
+  alias: {
+    '@': path.resolve(__dirname),
+  },
+  css: [
+  ],
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  modules: [
+    ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }],
+    '@vueuse/nuxt',
+    'nuxt-quasar-ui',
+  ],
+  experimental: {
+    viewTransition: true,
+  },
+});
