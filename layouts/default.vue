107af2a75290f10e0711b1ec4e294124130ebde5
Ixe Fernández <feguedi@yahoo.com.mx>
2024-07-01 18:07:49 -0500

    Creados página y layout para inicio

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    <QLayout>
+      <QPageContainer class="window-width window-height">
+        <QPage class="fit row items-center justify-center content-center">
+          <slot />
+        </QPage>
+      </QPageContainer>
+    </QLayout>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Zorros Football Academy',
+});
+</script>
+
+<template>
+  <div class="col-10 col-md-6 col-lg-8">
+    <div class="column q-pa-none q-pa-lg-lg">
+      <QImg src="/logo.svg" fit="contain" style="max-height: 80vh" alt="Logo de Zorros Football Academy" />
+    </div>
+  </div>
+</template>
