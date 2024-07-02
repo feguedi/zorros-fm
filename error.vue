49b4a868bb5513e29d8bfea6fc66d68e8c9f4540
Ixe Fernández <feguedi@yahoo.com.mx>
2024-07-02 03:45:17 -0500

    Creada vista para mostrar errores

---
+++ error.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: '/' });
+
+useHead({
+  title: 'Error | Zorros Football Academy',
+});
+</script>
+
+<template>
+  <div>
+    <QLayout>
+      <QPageContainer class="window-width window-height">
+        <QPage class="fit row items-center justify-center content-center">
+          <div class="col-10 col-md-6 col-lg-8">
+            <div class="column q-pa-none q-pa-lg-lg">
+              <h2>{{ error.statusCode }}</h2>
+
+              <p>{{ error.message }}</p>
+
+              <QBtn color="primary" label="Limpiar errores" @click="handleError" />
+            </div>
+          </div>
+        </QPage>
+      </QPageContainer>
+    </QLayout>
+  </div>
+</template>
