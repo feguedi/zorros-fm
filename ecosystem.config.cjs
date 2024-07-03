c07b427f79ffdf422f7cc31bd123e59bbbfa7da7
Ixe Fernández <feguedi@yahoo.com.mx>
2024-07-03 01:08:33 -0500

    Actualizado archivo de PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,6 @@ module.exports = {
     {
       name: 'zorros-fm-client',
       port: '3030',
-      exec_mode: 'cluster',
-      instances: 'max',
       script: './.output/server/index.mjs',
     },
   ],
