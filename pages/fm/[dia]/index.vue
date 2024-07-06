1de9c10e19f796e3e261d818f87ecae850e67b96
Ixe Fernández <feguedi@yahoo.com.mx>
2024-07-05 23:25:41 -0500

    Creada vista principal para administrador de archivos
    - Creada vista para día que se está viendo video

---
+++ pages/fm/[dia]/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'user-main',
+  middleware: 'auth',
+});
+
+useHead({
+  title: 'Zorros Football Academy',
+});
+</script>
+
+<template>
+  <div></div>
+</template>
---
+++ pages/fm/index.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+type team = 'lions' | 'sharks' | 'aguilasdoradas' | 'bufalos' | 'cachorros' | 'diablos' | 'jaguares';
+const equipos = ref<team[]>([
+  'lions',
+  'sharks',
+  'aguilasdoradas',
+  'bufalos',
+  'cachorros',
+  'diablos',
+  'jaguares',
+]);
+
+definePageMeta({
+  layout: 'user-main',
+  middleware: 'auth',
+});
+
+useHead({
+  title: 'Zorros Football Academy',
+});
+</script>
+
+<template>
+  <div class="row q-mx-xl q-mt-xl">
+
+    <!--
+    <template v-for="t in equipos" :key="t">
+      <div class="col-4 bg-green-5">
+        <IconLogo :team="t" />
+      </div>
+    </template>
+    -->
+  </div>
+</template>
