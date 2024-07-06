7cffd982b11ea754531494f2426328083bddc56c
Ixe Fernández <feguedi@yahoo.com.mx>
2024-07-05 23:24:01 -0500

    Creado layout para usuarios

---
+++ components/Main/Layout.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
+
+<template>
+  <QLayout view="hHh lpR fFf">
+    <QHeader elevated class="bg-primary text-white">
+      <QToolbar>
+        <QBtn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <QToolbarTitle>
+          <QAvatar class="q-mr-lg">
+            <img src="/public/img/academia-zorros-logo_naranja.svg">
+          </QAvatar>
+
+          <slot name="title" />
+        </QToolbarTitle>
+
+        <div class="row q-ml-md-auto">
+          <div class="col">
+            <slot name="menus" />
+          </div>
+        </div>
+      </QToolbar>
+    </QHeader>
+
+    <QDrawer v-model="leftDrawerOpen" side="left" overlay behavior="mobile" bordered>
+      <slot name="leftDrawer" />
+    </QDrawer>
+
+    <QPageContainer>
+      <slot />
+    </QPageContainer>
+  </QLayout>
+</template>
---
+++ layouts/user/main.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    <MainLayout>
+      <template #title>Inicio</template>
+
+      <template #default>
+        <slot />
+      </template>
+    </MainLayout>
+  </div>
+</template>
