503a066adee0636f837dd2fb8210fd6a7408385a
Ixe Fernández <feguedi@yahoo.com.mx>
2024-07-05 23:24:17 -0500

    Creada vista y composable de recuperación de contraseña

---
+++ composables/useRecover.ts
@@ -0,0 +1,27 @@
+export async function useRecover() {
+  const $q = useQuasar();
+  const telefono = ref<string>('');
+
+  async function fetchRecover() {
+    try {
+      // TODO: agregar implementación de recuperación de contraseña
+      return true;
+    } catch (error: any) {
+      console.error('fetchRecover Error:', error);
+
+      if ($q.notify) {
+        $q.notify({ message: `Error ${error.message || 'en la solicitud de recuperación'}`, type: 'negative' });
+      }
+    }
+  }
+
+  function resetValues() {
+    telefono.value = '';
+  }
+
+  return {
+    telefono,
+    fetchRecover,
+    resetValues,
+  };
+}
---
+++ pages/auth/recover.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+import { useRecover } from '~/composables/useRecover';
+
+const { telefono, fetchRecover, resetValues } = useRecover();
+
+definePageMeta({
+  middleware: 'auth',
+  layout: 'auth',
+});
+
+useHead({
+  title: 'Recuperar contraseña | Zorros Football Academy',
+});
+</script>
+
+<template>
+  <div>
+    <div class="q-mb-lg column">
+      <QImg
+        class="self-center"
+        src="/img/academia-zorros-logo_bco.svg"
+        loading="lazy"
+        width="400px"
+        spinner-color="white"
+      />
+    </div>
+
+    <QForm
+      @submit.prevent="fetchRecover"
+      @reset="resetValues"
+      class=""
+    >
+      <QInput
+        v-model="telefono"
+        filled
+        autofocus
+        spellcheck="false"
+        autocomplete="off"
+        label="Usuario"
+        lazy-rules
+        :rules="[(val: string) => val && val.length > 0 || 'El usuario no puede estar vacío']"
+      />
+
+      <div class="row justify-center content-around items-center">
+        <QBtn label="Recuperar" type="submit" color="primary" />
+        <QBtn label="Borrar" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </QForm>
+  </div>
+</template>
