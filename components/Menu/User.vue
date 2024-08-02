2a23208f33facbf82c75b9335d0053947439f52a
Ixe Fernández <feguedi@yahoo.com.mx>
2024-08-02 18:05:50 -0500

    Creado MenuUser

---
+++ components/Menu/User.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const { fetchLogout } = useLogout();
+</script>
+
+<template>
+  <QBtn round color="primary" icon="fa-user">
+    <QMenu>
+      <QList dense style="min-width: 100px">
+        <QItem clickable>
+          <QItemSection>
+            <NuxtLink to="/profile">Mis datos</NuxtLink>
+          </QItemSection>
+        </QItem>
+
+        <QSeparator />
+
+        <QItem clickable v-close-popup>
+          <QItemSection>
+            <QBtn label="Cerrar sesión" @click="() => fetchLogout()" />
+          </QItemSection>
+        </QItem>
+      </QList>
+    </QMenu>
+  </QBtn>
+</template>
