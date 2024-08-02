e639ca1f13a2dc6156a07cec763d8c38bf43e8c3
Ixe Fernández <feguedi@yahoo.com.mx>
2024-08-02 18:05:28 -0500

    Creada página para ver datos de usuario

---
+++ pages/profile/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const perfilStore = usePerfilStore();
+
+useHead({
+  title: 'Mis datos | Zorros Football Academy',
+});
+
+definePageMeta({
+  auth: {
+    unauthenticatedOnly: false,
+    navigateUnauthenticatedTo: '/auth/login',
+  },
+  layout: 'user-main',
+});
+</script>
+
+<template>
+  <div>
+    {{ JSON.stringify(perfilStore.datos, null, 2) }}
+  </div>
+</template>
